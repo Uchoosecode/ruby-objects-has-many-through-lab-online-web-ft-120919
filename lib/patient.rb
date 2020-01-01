12465507d1f79ba6c6a124f18ec83426826b008c
Ayako Wiltshire <awittyl44@gmail.com>
2020-01-01 18:00:51 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,10 +4,16 @@ class Patient
   
   attr_accessor :name, :appointment, :doctor
   
+  @@all = []
+  
   def initialize(name)
     @name = name
   end
     
+  def self.all
+    @@all
+  end
+  
   def new_appointment(doctor, date)
     Appointment.new(date, self, doctor)
   end
